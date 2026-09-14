11cdcc5d7f6a83be38845234efda95ec40cb75f5
leovit <leonardogondor@gmail.com>
2026-09-14 19:40:12 -0300

    ddddddd

--- app/components/CardDestino.tsx
+++ app/components/CardDestino.tsx
@@ -1,6 +1,8 @@
 import Link from "next/link";
 import { Destino } from "../types/Destino";
 import styles from "./CardDestino.module.css";
+// uso de imagem otimizada com next/image para melhor performance e SEO
+import Image from "next/image";
 
 interface CardDestinoProps {
     destino: Destino;
@@ -9,9 +11,10 @@ interface CardDestinoProps {
 export default function CardDestino({ destino }: CardDestinoProps) {
     return (
         <div className={styles.card}>
-            <img src={destino.imagem} alt={destino.nome} className={styles.image} />
+            // uso de imagem otimizada com next/image para melhor performance e SEO
+            <Image src={destino.imagem} alt={destino.nome} className={styles.image} width={300} height={200} loading="lazy" />
             <h2 className="text-2xl mb-2">{destino.nome}</h2>
-            <Link  className="text-xl text-blue-600 hover:text-blue-300" href={`/destinos/${destino.id}`}>Ver detalhes</Link>
+            <Link className="text-xl text-blue-600 hover:text-blue-300" href={`/destinos/${destino.id}`}>Ver detalhes</Link>
         </div>
     );
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,12 @@ import type { Metadata } from "next";
 import "./globals.css";
 import React from "react";
 
+// Descrição e palavras-chave para melhorar SEO:
 export const metadata: Metadata = {
     title: "Portal de Viagens",
-};
+    description: "Descubra destinos incríveis pelo mundo e planeje sua próxima aventura com nosso portal de viagens.",
+    keywords: "viagens, turismo, destinos, aventuras, férias, viagens internacionais, viagens nacionais, dicas de viagem",
+};  
 
 export default function RootLayout({
     children,
