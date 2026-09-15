232741aa2252dea35ef9918c2f243633c4b182d2
leovit <leonardogondor@gmail.com>
2026-09-14 22:13:45 -0300

    dkdkdk

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,14 @@ export const metadata: Metadata = {
     title: "Portal de Viagens",
     description: "Descubra destinos incríveis pelo mundo e planeje sua próxima aventura com nosso portal de viagens.",
     keywords: "viagens, turismo, destinos, aventuras, férias, viagens internacionais, viagens nacionais, dicas de viagem",
+    openGraph: {
+        title: "Portal de Viagens",
+        description: "Planeje sua próxima aventura com nosso portal.",
+        url: "https://portal-viagens-s6ra-git-master-leo-vit.vercel.app/",
+        siteName: "Portal de Viagens",
+        locale: "pt_BR",
+        type: "website",
+    },
 };  
 
 export default function RootLayout({
