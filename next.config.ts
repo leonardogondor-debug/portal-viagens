cfcea2dedf222d4c9bad35eb39fbc57ad0beda70
leovit <leonardogondor@gmail.com>
2026-09-14 21:40:55 -0300

    compressao

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,45 @@
 import type { NextConfig } from "next";
+import { sources } from "next/dist/compiled/webpack/webpack";
+import { redirect } from "next/dist/server/api-utils";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+
+  // compressao gzip/brotli
+  compress: true,
+
+  //confg de imagem
+  images: {
+    formats: ["image/webp"],
+    deviceSizes: [320, 640, 768, 1024, 1200],
+  },
+
+  //headers para preload e cache
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "public, max-age=31536000, immutable",
+          },
+        ],
+      },
+    ];
+  },
+
+  // redirecionamento se preciso
+  async redirects() {
+    return [
+      {
+        source: "/old-route",
+        destination: "/new-route",
+        permanent: true,
+      },
+    ];
+  },
 };
+
 
 export default nextConfig;
